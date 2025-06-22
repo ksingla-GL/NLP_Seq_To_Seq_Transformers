27144e17a7764fa6c468f1c00393d8c6adf21b8e
Kshitij Singla <80511199+ksingla-GL@users.noreply.github.com>
2025-06-22 15:28:53 +0530

    Update Seq2Seq.py

--- models/seq2seq/Seq2Seq.py
+++ models/seq2seq/Seq2Seq.py
@@ -1,27 +1,4 @@
 import random
-
-""" 			  		 			     			  	   		   	  			  	
-Seq2Seq model.  (c) 2021 Georgia Tech
-
-Copyright 2021, Georgia Institute of Technology (Georgia Tech)
-Atlanta, Georgia 30332
-All Rights Reserved
-
-Template code for CS 7643 Deep Learning
-
-Georgia Tech asserts copyright ownership of this template and all derivative
-works, including solutions to the projects assigned in this course. Students
-and other users of this template code are advised not to share it with others
-or to make it available on publicly viewable websites including repositories
-such as Github, Bitbucket, and Gitlab.  This copyright statement should
-not be removed or edited.
-
-Sharing solutions with current or future students of CS 7643 Deep Learning is
-prohibited and subject to being investigated as a GT honor code violation.
-
------do not edit anything above this line---
-"""
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -32,24 +9,16 @@ import torch.optim as optim
 
 class Seq2Seq(nn.Module):
     """ The Sequence to Sequence model.
-        You will need to complete the init function and the forward function.
     """
 
     def __init__(self, encoder, decoder, device):
         super(Seq2Seq, self).__init__()
         self.device = device
         #############################################################################
-        # TODO:                                                                     #
-        #    Initialize the Seq2Seq model. You should use .to(device) to make sure  #
-        #    that the models are on the same device (CPU/GPU). This should take no  #
-        #    more than 2 lines of code.                                             #
+        #    Initialize the Seq2Seq model.                                          #
         #############################################################################
         self.encoder = encoder.to(device)
         self.decoder = decoder.to(device)
-
-        #############################################################################
-        #                              END OF YOUR CODE                             #
-        #############################################################################
 
     def forward(self, source):
         """ The forward pass of the Seq2Seq model.
@@ -60,11 +29,8 @@ class Seq2Seq(nn.Module):
         batch_size = source.shape[0]
         seq_len = source.shape[1]
         #############################################################################
-        # TODO:                                                                     #
-        #   Implement the forward pass of the Seq2Seq model. Please refer to the    #
-        #   following steps:                                                        #
-        #       1) Get the last hidden representation from the encoder. Use it as   #
-        #          the first hidden state of the decoder                            #
+        #       1) Will Get the last hidden representation from the encoder & use it#
+        #          as the first hidden state of the decoder                         #
         #       2) The first input for the decoder should be the <sos> token, which #
         #          is the first in the source sequence.                             #
         #       3) Feed this first input and hidden state into the decoder          #  
@@ -85,7 +51,4 @@ class Seq2Seq(nn.Module):
             out,hidden = self.decoder.forward(source,hidden,enc_outs)
             outputs = torch.concat((outputs,out.unsqueeze(1)),dim = 1)
             source = out.argmax(1).reshape(out.shape[0],1)
-        #############################################################################
-        #                              END OF YOUR CODE                             #
-        #############################################################################
         return outputs
